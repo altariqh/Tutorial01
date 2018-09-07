bb368f807f3816c4f9e2a8f4aa008026e50e71dc
altariqh <altariqhd@gmail.com>
2018-09-07 22:07:00 +0700

    Initial commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,18 @@
+
+public class Mahasiswa {
+	public String nama;
+	public int npm;
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public int getNpm() {
+		return npm;
+	}
+	public void setNpm(int npm) {
+		this.npm = npm;
+	}
+}
