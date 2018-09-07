8516d8f9094628300521713d4d6ad0758dc30b16
altariqh <altariqhd@gmail.com>
2018-09-07 22:10:24 +0700

    add Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,19 @@
+
+public class Dosen {
+	public String nama;
+	public int nik;
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public int getNik() {
+		return nik;
+	}
+	public void setNik(int nik) {
+		this.nik = nik;
+	}
+	
+	
+}
